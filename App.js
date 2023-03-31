21fa53b8c6a41f21e6b2402026b6aead8495fc0b
HemanthMotwani <hemanthmotwani@gmail.com>
2023-03-31 15:08:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TaskTracker from './TaskTracker';
+
+
+
+function App() {
+
+  return (
+    <div className='' Name="App">
+      <TaskTracker/>
+
+
+
+      
+    
+
+
+
+
+    </div>
+  );
+}
+
+
+export default App;
